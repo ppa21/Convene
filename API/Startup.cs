f62871945c24203ccfae6a5134edaacc13c969c8
Pruthviraj Patel <pruthvirajpatel@Pruthvirajs-MBP.vs.shawcable.net>
2020-05-09 23:16:54 -0700

    Handlers Added

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,13 @@
-using System;
 using Persistence;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Application.Activities;
 
 namespace API
 {
@@ -38,7 +34,8 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-            services.AddControllers();
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
